f5ed0d2678cc13ca79c0c5382f326eaa4534045b
Jacob-Walton <jacob-walton@konpeki.co.uk>
2026-04-24 16:39:35 +0100

    feat: initial commit

---
+++ Runshaw Bus/App/Runshaw_BusApp.swift
@@ -0,0 +1,17 @@
+//
+//  Runshaw_BusApp.swift
+//  Runshaw Bus
+//
+//  Created by Jacob on 24/04/2026.
+//
+
+import SwiftUI
+
+@main
+struct Runshaw_BusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Runshaw BusTests/Runshaw_BusTests.swift
@@ -0,0 +1,13 @@
+//
+//  Runshaw_BusTests.swift
+//  Runshaw BusTests
+//
+//  Created by Jacob on 24/04/2026.
+//
+
+import XCTest
+@testable import Runshaw_Bus
+
+final class BusesTests: XCTestCase {
+    
+}
